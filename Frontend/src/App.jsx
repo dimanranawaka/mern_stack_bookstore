178428bfb703b488d1d52d40be90d56849dd9fa5
dimanranawaka <dimanranawaka@gmail.com>
2024-05-14 22:37:41 +0530

    implement banner

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Banner />
+    </>
+  );
 }
 
 export default App;
