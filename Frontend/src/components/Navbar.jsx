4746dcba12a56728b56267ba2ddd51a762f96eb9
dimanranawaka <dimanranawaka@gmail.com>
2024-05-14 17:27:49 +0530

    implement navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
     </>
   );
   return (
-    <div>
+    <div className="max-w-screen-2x1 container mx-auto md:px-20 px-4">
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
@@ -47,12 +47,31 @@ function Navbar() {
           </div>
           <a className="text-2xl font-bold cursor-pointer">Bookstore</a>
         </div>
-        <div className="nabar-end">
+        <div className="nabar-end space-x-6">
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">{navItems}</ul>
           </div>
-          <div className="navbar-end">
-            <a className="btn">Button</a>
+          <div className="hidden md:block">
+            <label className="input input-bordered flex items-center gap-2">
+              <input type="text" className="grow" placeholder="Search" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </label>
+          </div>
+          <div className="">
+            <a className="bg-black text-white p-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer">
+              Login
+            </a>
           </div>
         </div>
       </div>
