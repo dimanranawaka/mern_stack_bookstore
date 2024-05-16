8d33c919a4e2bb09b34e801ef021d6a82d090822
dimanranawaka <dimanranawaka@gmail.com>
2024-05-16 20:51:35 +0530

    content updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import Footer from "./components/Footer";
-import Freebook from "./components/Freebook";
+import Home from "./Home/Home";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Banner />
-      <Freebook />
-      <Footer />
+      <Home />
     </>
   );
 }
--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Course() {
-  return <div></div>;
+  return <></>;
 }
 
 export default Course;
