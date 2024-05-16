c74627b0a93119f0e9d0e054c7fbde526a7c5a84
dimanranawaka <dimanranawaka@gmail.com>
2024-05-16 22:34:44 +0530

    adding courses

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Home from "./Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Course from "./components/Course";
+import Courses from "./Courses/Courses";
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="course" element={<Courses />} />
+      </Routes>
     </>
   );
 }
---
+++ Frontend/src/Courses/Courses.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Courses() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default Courses;
