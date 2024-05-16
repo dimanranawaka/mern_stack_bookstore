db76413b0338d2a45c7ed0c02fff3e53ae34b248
dimanranawaka <dimanranawaka@gmail.com>
2024-05-16 16:24:40 +0530

    implement Freebook

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import list from "../assets/list.json";
 
 function Freebook() {
+  const filterData = list.filter((data) => data.category === "Free");
+  console.log(filterData);
   return <div></div>;
 }
 
