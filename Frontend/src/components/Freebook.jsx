13c71035d2ba39df118218d8be9786259f48c98c
dimanranawaka <dimanranawaka@gmail.com>
2024-05-16 18:54:49 +0530

    implement cards

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -4,6 +4,7 @@ import list from "../assets/list.json";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Cards from "./Cards";
 
 function Freebook() {
   const filterData = list.filter((data) => data.category === "Free");
@@ -57,32 +58,11 @@ function Freebook() {
           </p>
         </div>
 
-        <div>
+        <div className="slider-container">
           <Slider {...settings}>
-            <div>
-              <h3>1</h3>
-            </div>
-            <div>
-              <h3>2</h3>
-            </div>
-            <div>
-              <h3>3</h3>
-            </div>
-            <div>
-              <h3>4</h3>
-            </div>
-            <div>
-              <h3>5</h3>
-            </div>
-            <div>
-              <h3>6</h3>
-            </div>
-            <div>
-              <h3>7</h3>
-            </div>
-            <div>
-              <h3>8</h3>
-            </div>
+            {filterData.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
           </Slider>
         </div>
       </div>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ function Navbar() {
 
   return (
     <div
-      className={`max-w-screen-2xl container mx-auto md:px-10 px-4 fixed top-0 left-0 right-0 ${
+      className={`max-w-screen-2xl container mx-auto md:px-10 px-4 fixed top-0 left-0 right-0 z-50 ${
         sticky
           ? "sticky-navbar shadow-lg bg-base-200 duration-300 trasition-all ease-in-out"
           : ""
