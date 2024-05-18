218b59ebab6d4397312661df658752d7b6c24802
dimanranawaka <dimanranawaka@gmail.com>
2024-05-18 14:51:13 +0530

    adding scaling effect

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -6,7 +6,7 @@ function Cards({ item }) {
   return (
     <>
       <div className="mt-4 my-3">
-        <div className="card w-92 p-3  bg-base-100 shadow-xl">
+        <div className="card w-92 p-3  bg-base-100 shadow-xl hover:scale-105 duration-200">
           <figure>
             <img src={item.image} alt="Shoes" />
           </figure>
