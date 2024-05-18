4b5d37b8b401673916b9e0b987c051efdcaee434
dimanranawaka <dimanranawaka@gmail.com>
2024-05-18 14:43:04 +0530

    implement course

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -21,7 +21,7 @@ function Course() {
             Back
           </button>
         </div>
-        <div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
           {list.map((item) => {
             return <Cards key={item.id} item={item} />;
           })}
