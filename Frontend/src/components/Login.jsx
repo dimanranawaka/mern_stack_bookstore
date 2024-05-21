d8839d3d76ca75031fa1db886ebbe4799822a427
dimanranawaka <dimanranawaka@gmail.com>
2024-05-21 13:38:02 +0530

    bug fixed

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,59 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <>
       <div>
-        {/* You can open the modal using document.getElementById('ID').showModal() method */}
-
         <dialog id="my_modal_3" className="modal">
           <div className="modal-box">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <Link
-                to="/"
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+              <button
                 className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                onClick={() => document.getElementById("my_modal_3").close()}
               >
                 ✕
-              </Link>
-            </form>
-            <h3 className="font-bold text-lg">Login</h3>
-            {/* Email */}
-            <div>
-              <span>Email</span>
-              <br />
-              <input
-                type="email"
-                placeholder="Enter Your Email"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
-              />
-            </div>
-            {/* Password */}
-            <div>
-              <span>Password</span>
-              <br />
-              <input
-                type="text"
-                placeholder="Enter Your Password"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
-              />
-            </div>
-            {/* Button */}
-            <div className="flex justify-around mt-4">
-              <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
-                Login
               </button>
-              <p>
-                Not registered?{" "}
-                <Link
-                  to="/signup"
-                  className="underline text-blue-500 cursor-pointer"
-                >
-                  SignUp
-                </Link>{" "}
-              </p>
-            </div>
+              <h3 className="font-bold text-lg">Login</h3>
+              <div>
+                <span>Email</span>
+                <br />
+                <input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                  {...register("email", { required: true })}
+                />
+                {errors.email && <span>This field is required</span>}
+              </div>
+              <div>
+                <span>Password</span>
+                <br />
+                <input
+                  type="password"
+                  placeholder="Enter Your Password"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                  {...register("password", { required: true })}
+                />
+                {errors.password && <span>This field is required</span>}
+              </div>
+              <div className="flex justify-around mt-4">
+                <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                  Login
+                </button>
+                <p>
+                  Not registered?{" "}
+                  <Link
+                    to="/signup"
+                    className="underline text-blue-500 cursor-pointer"
+                  >
+                    SignUp
+                  </Link>{" "}
+                </p>
+              </div>
+            </form>
           </div>
         </dialog>
       </div>
